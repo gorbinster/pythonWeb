9c74a2e271b8db09dfc391e0ba67f3275d219126
gorbinster <Gorbin@Yun-BodeMacBook-Pro.local>
2017-01-24 22:38:27 +0800

    添加注释

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 # coding=utf-8
-from flask import Flask
+from flask import Flask				#调用 Flask
 
-app = Flask(__name__)
+app = Flask(__name__)				#建立 Flask 实例
 
-@app.route('/')
+@app.route('/')						#路由建立 url 和视图函数的映射
 def index():
 	return '<h1>Hello World!<h1/>'
 
-@app.route('/user/<name>')
+@app.route('/user/<name>')			#动态 url
 def user(name):
-	return '<h1>Hello, %s!<h1/>' % name
+	return '<h1>Hello, %s!<h1/>' % name, 200 # 视图函数和状态码（可选）
 
 if __name__ == '__main__':
 	app.run(debug=True)
